563c858add5afabbe3973ba87e30206224f96b11
rogoz208 <rogoz208@gmail.com>
2021-12-20 18:55:21 +0300

    lesson-1: more code refactoring

--- app/src/main/java/com/example/nasapicture/domain/repos/NasaPodRepo.kt
+++ app/src/main/java/com/example/nasapicture/domain/repos/NasaPodRepo.kt
@@ -3,7 +3,10 @@ package com.example.nasapicture.domain.repos
 import com.example.nasapicture.domain.entities.NasaPodEntity
 
 interface NasaPodRepo {
-    fun getPictureOfTheDay(
+
+    fun getPictureOfTheDaySync(): NasaPodEntity
+
+    fun getPictureOfTheDayAsync(
         onSuccess: (NasaPodEntity) -> Unit,
         onError: (Throwable) -> Unit
     )
--- app/src/main/java/com/example/nasapicture/ui/screens/picture/NasaPodFragment.kt
+++ app/src/main/java/com/example/nasapicture/ui/screens/picture/NasaPodFragment.kt
@@ -1,8 +1,8 @@
 package com.example.nasapicture.ui.screens.picture
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.bumptech.glide.Glide
@@ -15,9 +15,7 @@ class NasaPodFragment : Fragment(R.layout.fragment_nasa_pod) {
     private val binding by viewBinding(FragmentNasaPodBinding::bind)
 
     private val viewModel: NasaPodContract.ViewModel by viewModels {
-        NasaPodViewModelFactory(
-            requireContext().app.nasaPodRepo
-        )
+        NasaPodViewModelFactory(requireContext().app.nasaPodRepo)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -27,7 +25,7 @@ class NasaPodFragment : Fragment(R.layout.fragment_nasa_pod) {
     }
 
     private fun initViewModel() {
-        viewModel.nasaPodLiveData.observe(this) { nasaPictureEntity->
+        viewModel.nasaPodLiveData.observe(this) { nasaPictureEntity ->
             Glide.with(this).load(nasaPictureEntity.url).into(binding.nasaPictureImageView)
             binding.nasaPictureDescriptionTextView.text = nasaPictureEntity.explanation
         }
